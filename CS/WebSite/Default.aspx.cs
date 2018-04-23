a5be8f78958c418fd8aecf581207ee0009e9c303
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DevExpress.Web.ASPxGridView;
+
+public partial class _Default : System.Web.UI.Page {
+    protected void Page_Load (object sender, EventArgs e) {
+
+    }
+    protected void ASPxGridView1_DataBound (object sender, EventArgs e) {
+        ASPxGridView grid = sender as ASPxGridView;
+        if (grid.Columns.IndexOf(grid.Columns["CommandColumn"]) != -1)
+            return;
+        GridViewCommandColumn col = new GridViewCommandColumn();
+        col.Name = "CommandColumn";
+        col.ShowSelectCheckbox = true;
+        col.VisibleIndex = 0;
+        grid.Columns.Add(col);
+
+    }
+}
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,27 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports DevExpress.Web.ASPxGridView
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+
+	End Sub
+	Protected Sub ASPxGridView1_DataBound(ByVal sender As Object, ByVal e As EventArgs)
+		Dim grid As ASPxGridView = TryCast(sender, ASPxGridView)
+		If grid.Columns.IndexOf(grid.Columns("CommandColumn")) <> -1 Then
+			Return
+		End If
+		Dim col As New GridViewCommandColumn()
+		col.Name = "CommandColumn"
+		col.ShowSelectCheckbox = True
+		col.VisibleIndex = 0
+		grid.Columns.Add(col)
+
+	End Sub
+End Class
